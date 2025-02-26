95ee0fd1f1f9eb18e29519fd6ee38495c948bcd1
dibasdauliya <dibasdauliya@gmail.com>
2025-02-26 17:18:49 -0500

    setup ci/cd

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 import nextra from "nextra";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
@@ -20,19 +15,6 @@ const nextConfig = {
     unoptimized: true,
   },
   distDir: "out",
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.cache = {
-        type: "filesystem",
-        cacheDirectory: path.resolve(__dirname, ".webpack_cache"),
-        store: "pack",
-        buildDependencies: {
-          config: [__filename],
-        },
-      };
-    }
-    return config;
-  },
 };
 
 export default withNextra(nextConfig);
